11290b63fec52cb956d47ea4a7c959936e382617
Nai <nailahmukhtar@Nailahs-Air.home>
2023-01-27 20:20:24 +0000

    updated application to change text areas colour to show past, present and future timeblocks

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,2 +1,22 @@
 var today = moment().format("dddd, Do MMMM YYYY");
-$('#currentDay').text(today);
+$("#currentDay").text(today);
+
+var currentHour = moment().hour();
+var timeblock = $(".row");
+
+//Loop through timeblock html rows
+for (let i = 0; i < timeblock.length; i++) {
+    //get current textarea & text areaid for comparison
+    var currentTimeBlock = timeblock.eq(i);
+    var blockId = parseInt(currentTimeBlock.attr("id"));
+    var textArea = currentTimeBlock.children("textarea");
+
+    //compare current textareaid with currenthour
+    if (blockId === currentHour) {
+        textArea.addClass("present");
+    } else if (blockId > currentHour) {
+        textArea.addClass("future");
+    } else {
+        textArea.addClass("past");
+    }
+}
